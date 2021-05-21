ba438116bc048fd1077c28975922e48324cd7631
Seoin <68271159+tjdls111@users.noreply.github.com>
2021-05-21 16:54:56 +0900

    색깔로 필터링됨

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 'use strict';
 
 const Main=document.querySelector('.box');
+const buttons=document.querySelector('nav');
+
+
 console.log(Main);
 function addList(gender,size,color,style)
 {
     const item=document.createElement('li');
-    item.setAttribute('class',color);
-    item.setAttribute('class',style);
-    item.setAttribute('class','list_item')
+    item.setAttribute('class', 'list_item');
+    item.className+=` ${color}`;
+    item.className+=` ${style}`;
+    if (style=='p'){
+        item.className+=` pants`;
+    }
+    else if (style=='s'){
+        item.className+=` skirt`;
+    }
+    else if (style=='t'){
+        item.className+=` tshirt`;
+    }
     item.innerHTML=`<img src="imgs/${color}_${style}.png" alt="">
        ${gender}, ${size} size`;
     Main.appendChild(item);
@@ -17,38 +29,39 @@ addList('male','small','blue','p');
 addList('female','large','pink','s');
 addList('male','xsmall','yellow','t');
 addList('male','xlarge','pink','p');
-addList('female','xxlarge','yellow','p');
-addList('female','xxlarge','yellow','p');
+addList('female','xxlarge','yellow','s');
+addList('female','xxlarge','yellow','s');
 addList('male','large','blue','t');
 addList('male','large','blue','t');
-addList('female','small','blue','s');
-addList('female','medium','yellow','s');
+addList('female','small','blue','p');
+addList('female','medium','yellow','p');
 
 
 function select(colorOrStyle){
-    for (let i in Main){
-        console.log(i.value);
-        /*if (i.class!=colorOrStyle){
-            i.setAttribute('style','visibility:hidden');
+
+    const items=document.getElementsByTagName('li');
+    console.log(items);
+    for (let i of items){
+        console.log(i.className);
+        if (i.className.includes(colorOrStyle)){
+            i.setAttribute('style','visibility:visible');
         }
         else {
-            i.setAttribute('style','visibility:visible');
-        }*/
-    }
-}
+            i.setAttribute('style','visibility:hidden');
+        }
+      }};
+    
 
 const pantsBtn=document.querySelector('.pantsBtn');
 const skirtBtn=document.querySelector('.skirtBtn');
 const tshirtBtn=document.querySelector('.tshirtBtn');
 
-const pinkBtn=document.querySelector('.pinkBtn');
-const yellowBtn=document.querySelector('.yellowBtn');
-const blueBtn=document.querySelector('.blueBtn');
-select('pants');
+const pinkBtn=document.querySelector('.pink');
+const yellowBtn=document.querySelector('.yellow');
+const blueBtn=document.querySelector('.blue');
 
-pantsBtn.addEventListener('click',()=>{
-    select('pants');
+buttons.addEventListener('click',(event)=>{
+    console.log(event.target.classList.value);
+    select(event.target.classList.value);
+    
 });
-
-
-
